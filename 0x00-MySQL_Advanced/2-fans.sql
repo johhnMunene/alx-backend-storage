98b0c9b05a25231376152bf98b62b260af079d1d
johhnMunene <Edwardsjohnny64@gmail.com>
2024-04-27 08:59:43 -0500

    2-fans.sql uodates

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,7 @@
 -- A  SQL script that ranks country origins of bands, ordered
 -- by the number of (non-unique) fans
 
-SELECT origin,SUM(nb_fans) AS total_fans
-FROM metal bands
-GROUP BY origin;
+-- script that ranks country origins of bands, ordered
+-- by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans from metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
