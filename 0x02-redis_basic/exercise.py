809532341b7712d098b87ce4bea982b38d1a0e60
johhnMunene <Edwardsjohnny64@gmail.com>
2024-05-01 12:56:07 -0500

    0x02. Redis basic updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,87 +1,107 @@
 #!/usr/bin/env python3
-
 """
-creating the cache class for the redis model
+Redis module
 """
-
-from uuid import uuid4
-import redis
-from typing import Union
-from collections.abc import Callable
+import sys
 from functools import wraps
+from typing import Union, Optional, Callable
+from uuid import uuid4
+
+import redis
+
+UnionOfTypes = Union[str, bytes, int, float]
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    a system to count how many
+    times methods of the Cache class are called.
+    :param method:
+    :return:
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrap
+        :param self:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    add its input parameters to one list
+    in redis, and store its output into another list.
+    :param method:
+    :return:
+    """
+    key = method.__qualname__
+    i = "".join([key, ":inputs"])
+    o = "".join([key, ":outputs"])
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapp """
+        self._redis.rpush(i, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(o, str(res))
+        return res
+
+    return wrapper
 
 
 class Cache:
-    """ cache"""
+    """
+    Cache redis class
+    """
 
     def __init__(self):
-        """ initializing the cache class"""
+        """
+        constructor of the redis model
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def count_calls(method: Callable) -> Callable:
-        """ creating a wrapper function"""
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            key = method.__qualname__
-            count_key = key + "_count"
-            self._redis.incr(count_key)
-            return method(self, *args, **kwargs)
-        return wrapper
-
-    def call_history(method: Callable) -> Callable:
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            inputs_key = f"{method.__qualname__}:inputs"
-            outputs_key = f"{method.__qualname__}:outputs"
-            self._redis.rpush(inputs_key, str(args))
-            result = method(self, *args, **kwargs)
-            self._redis.rpush(outputs_key, result)
-
-            return result
-
-        return wrapper
-
+    @count_calls
     @call_history
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ a class store that takes in data"""
-        id = str(uuid4())
+    def store(self, data: UnionOfTypes) -> str:
+        """
+        generate a random key (e.g. using uuid),
+         store the input data in Redis using the
+          random key and return the key.
+        :param data:
+        :return:
+        """
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
 
-        if isinstance(data, (int, float)):
-            data = str(data)
-        self._redis.set(id, data)
-        return id
-
-    def get(
-            self,
-            key: str,
-            fn: Callable[[], None] = None
-    ) -> Union[str, bytes, int, None]:
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> UnionOfTypes:
+        """
+        convert the data back
+        to the desired format
+        :param key:
+        :param fn:
+        :return:
+        """
+        if fn:
+            return fn(self._redis.get(key))
         data = self._redis.get(key)
-        if data is None:
-            return None
+        return data
 
-        if fn is not None:
-            return fn(data)
+    def get_int(self: bytes) -> int:
+        """get a number"""
+        return int.from_bytes(self, sys.byteorder)
 
-    def get_str(self, key: str) -> Union[str, bytes, None]:
-        """ get str function"""
-        return self.get(key, fn=lambda d: d.decode('utf-8'))
-
-    def get_int(self, key: str) -> Union[int, bytes, None]:
-        """ get int method"""
-        return self.get(key, fn=int)
-
-    def replay(self, method: Callable):
-        inputs_key = f"{method.__qualname__}:inputs"
-        outputs_key = f"{method.__qualname__}:outputs"
-
-        inputs = self._redis.lrange(inputs_key, 0, -1)
-        outputs = self._redis.lrange(outputs_key, 0, -1)
-
-        print(f"{method.__qualname__} was called {len(inputs)} times:")
-
-        for input_args, output in zip(inputs, outputs):
-            input_args = input_args.decode("utf-8")
-            output = output.decode("utf-8")
-            print(f"{method.__qualname__}(*{input_args}) -> {output}")
+    def get_str(self: bytes) -> str:
+        """get a string"""
+        return self.decode("utf-8")
